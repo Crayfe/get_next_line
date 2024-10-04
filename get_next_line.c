b8a0816e0b8cf9a95a58e377a347277cc4aa5ffa
cayuso-f <cayuso-f@student.42madrid.com>
2024-10-04 14:21:49 +0200

    first steps with the project

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,26 @@
 /*   By: cayuso-f <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 16:28:45 by cayuso-f          #+#    #+#             */
-/*   Updated: 2024/10/01 17:04:11 by cayuso-f         ###   ########.fr       */
+/*   Updated: 2024/10/04 14:08:27 by cayuso-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
+	static char	cache_line;
 	char	*next_line;
-	char	*c;
+	char	*buffer_line;
+	int	i;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return null;
-	while ()
+	buffer_line = (char *)malloc(BUFFER_SIZE);
+	i = 0;
+	while (read(fd, buffer_line + i, 1) > 0 && buffer_line[i] != '\n')
+	{
+		i++;
+	}
 
 	return (next_line);
 }
--- test_get_next_line.c
+++ test_get_next_line.c
@@ -1,39 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*   test_get_next_line.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cayuso-f <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/01 16:31:19 by cayuso-f          #+#    #+#             */
-/*   Updated: 2024/10/04 14:21:01 by cayuso-f         ###   ########.fr       */
+/*   Created: 2024/10/04 12:31:43 by cayuso-f          #+#    #+#             */
+/*   Updated: 2024/10/04 12:59:54 by cayuso-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
+#include <stdio.h>
 
-/*	AUXILIAR FUNCTIONS	*/
-size_t	ft_strlen(char *s)
-{
+int	main(void){
+
+	int	fd;
+	char	*line;
+
+	fd = open("file_test", 0);
+	if (fd > 0)
+	{
+		line = get_next_line(fd);
+		printf("Line: %s\n", line);
+		free(line);
+	}
+	return (0);
 }
-
-char	*ft_strchr(char *s, int c)
-{
-}
-
-char	*ft_substr(char *s, unsigned int start, size<-t len)
-{
-}
-
-size_t	ft_strlcpy(char *dst, char *src, size_t size)
-{
-}
-
-size_t	ft_strlcat(char *dst, char *src, size_t size)
-{
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-}
-
-
