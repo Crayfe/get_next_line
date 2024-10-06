bf23c1618ebc51d35ac91d7ccd3d77ccb73358d5
Cristian Ayuso <crayfe@gmail.com>
2024-10-06 10:58:43 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -14,12 +14,19 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 5
 # endif
 
 # include <unistd.h>
 # include <stdlib.h>
 
-char	*get_next_line(int fd);
+char	  *get_next_line(int fd);
+void	  *ft_memset(void *s, int c, size_t n);
+size_t	ft_strlen(const char *str);
+char	  *ft_strchr(const char *s, int c);
+char	  *ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	  *ft_strjoin(char const *s1, char const *s2);
 
 #endif
