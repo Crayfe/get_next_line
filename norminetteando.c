176f9b56a9a79faa90dbce532169a89ab93e3515
Cristian Ayuso <crayfe@gmail.com>
2024-10-09 10:18:38 +0200

    Update norminetteando.c

--- norminetteando.c
+++ norminetteando.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cayuso-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cayuso-f <cayuso-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 16:28:45 by cayuso-f          #+#    #+#             */
 /*   Updated: 2024/10/04 14:08:27 by cayuso-f         ###   ########.fr       */
@@ -12,88 +12,74 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-static char	*handle_nl_buffer(char *nl_buffer, char *next_line)
+char    *read_buffer(char *buffer, char c)
 {
-	char	*buffer_free;
+    char    *next_line;
+    int     pos;
 
-	if (!nl_buffer)
-	{
-		nl_buffer = (char *)malloc(1);
-		if (!nl_buffer)
-			return (0);
-		*nl_buffer = 0;
-	}
-	else
-	{
-		if (ft_strchr(nl_buffer, '\n'))
-		{
-			next_line = ft_substr(nl_buffer, 0, ft_strlen(nl_buffer, '\n') + 1);
-			buffer_free = nl_buffer;
-			nl_buffer = ft_substr(buffer_free, ft_strlen(nl_buffer, '\n')
-					+ 1, ft_strlen(nl_buffer, 0));
-			free(buffer_free);
-			return (nl_buffer);
-		}
-	}
-	return (nl_buffer);
+    pos = ft_strlen(buffer, c);
+    next_line = ft_substr(buffer, 0, pos + 1);
+    if (ft_strlen(buffer + pos, 0))
+
+    return (next_line);
 }
-
-static char	*read_nl(int fd, char *rd_buffer, char *nl_buffer, char *buffer_free)
+char    *get_next_line(int fd)
 {
-	int			num_char_read;
-	char		*next_line;
+    static char buffer[BUFFER_SIZE + 1];
+    char        *next_line;
+    char        *mem_free;
+    int         rd_chars;
 
-	rd_buffer = ft_memset(rd_buffer, 0, BUFFER_SIZE + 1);
-	num_char_read = read(fd, rd_buffer, BUFFER_SIZE);
-	if (num_char_read <= 0)
-	{
-		if (nl_buffer && ft_strlen(nl_buffer, 0) > 0)
-		{
-			next_line = ft_strjoin(nl_buffer, "\n");
-			free(nl_buffer);
-			nl_buffer = 0;
-			return (next_line);
-		}
-		free(nl_buffer);
-		return (0);
-	}
-	while (num_char_read == BUFFER_SIZE && !ft_strchr(rd_buffer, '\n'))
-	{
-		buffer_free = nl_buffer;
-		nl_buffer = ft_strjoin(nl_buffer, rd_buffer);
-		free(buffer_free);
-		rd_buffer = ft_memset(rd_buffer, 0, BUFFER_SIZE + 1);
-		num_char_read = read(fd, rd_buffer, BUFFER_SIZE);
-	}
-	buffer_free = ft_substr(rd_buffer, 0, ft_strlen(rd_buffer, '\n') + 1);
-	next_line = ft_strjoin(nl_buffer, buffer_free);
-	free(buffer_free);
-	free(nl_buffer);
-	nl_buffer = ft_substr(rd_buffer, ft_strlen(rd_buffer, '\n')
-			+ 1, ft_strlen(rd_buffer, 0));
-	free(rd_buffer);
-	return (0);
-}
-
-char	*get_next_line(int fd)
-{
-	static char	*nl_buffer;
-	char		*rd_buffer;
-	char		*buffer_free;
-	char		*next_line;
-
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-		return (0);
-	next_line = 0;
-	nl_buffer = handle_nl_buffer(nl_buffer, next_line);
-	if (!nl_buffer)
-		return (0);
-	if (next_line)
-		return (next_line);
-	rd_buffer = (char *)malloc(BUFFER_SIZE + 1);
-	if (!rd_buffer)
-		return (0);
-	buffer_free = 0;
-	next_line = read_nl(fd, rd_buffer, nl_buffer, buffer_free);
-	return (next_line);
+    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+        return (0);
+    next_line = 0;
+    if (ft_strlen(buffer, 0))
+    {
+        if (ft_strchr(buffer, '\n'))
+            next_line = read_buffer(buffer, '\n');
+        else
+            next_line = read_buffer(buffer, 0);
+    }
+    if (next_line && ft_strchr(next_line, '\n'))
+        return (next_line);
+    else
+    {
+        rd_chars = read(fd, buffer, BUFFER_SIZE
+        if (rd_chars <= 0)
+        {
+            if (next_line)
+            {
+                mem_free = next_line;
+                next_line = ft_strjoin(next_line, "\n");
+                free(mem_free);
+                return (next_line);
+            }
+            return (0);
+        }
+        while (rd_chars == BUFFER_SIZE && !ft_strchr(buffer, '\n'))
+        {
+            mem_free = next_line;
+            next_line = ft_strjoin(next_line, buffer);
+            free(mem_free);
+            buffer = ft_memset(buffer, 0, BUFFER_SIZE + 1);
+            rd_chars = read(fd, new_buffer, BUFFER_SIZE);
+        }
+        if (rd_chars < BUFFER_SIZE && !ft_strchr(new_buffer, '\n'))
+        {
+            mem_free = next_line;
+            next_line = ft_strjoin(next_line, buffer);
+            free(mem_free);
+            mem_free = next_line;
+            next_line = ft_strjoin(next_line, "\n");
+            free(mem_free);
+            buffer = ft_memset(buffer, 0, BUFFER_SIZE + 1);
+        }
+        else
+        {
+            mem_free = next_line;
+            next_line = ft_strjoin(next_line, read_buffer(buffer, '\n'));
+            free(mem_free);
+        }
+    }
+    return (next_line);
 }
