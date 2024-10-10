e095453ad33e0411aa319923ae477c0cdc32f45b
Crayfe <crayfe@gmail.com>
2024-10-10 23:02:02 +0200

    norminette approves .h

--- get_next_line.h
+++ get_next_line.h
@@ -20,14 +20,13 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-
-void	  *ft_memset(void *s, int c, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 size_t	ft_strlen(const char *str, char c);
-char	  *ft_strchr(const char *s, int c);
-char	  *ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
-char	  *ft_strjoin(char *s1, char const *s2);
-char	  *get_next_line(int fd);
+char	*ft_strjoin(char *s1, char const *s2);
+char	*get_next_line(int fd);
 
 #endif
