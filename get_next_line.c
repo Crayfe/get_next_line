a999e935be90d4bdea5f49415f3eb13abc4ca6c2
Crayfe <crayfe@gmail.com>
2024-10-10 11:59:22 +0200

    Project finally works

--- get_next_line.c
+++ get_next_line.c
@@ -20,22 +20,26 @@ char	*read_buffer(char *buffer, char c)
 
 	printf("\033[0;36mREAD BUFFER\n");
 	pos = ft_strlen(buffer, c);
+	printf("\033[0;36mBUFFER LEN: %i\n", pos);
+	printf("\033[0;36mBUFFER: %s\n", buffer);
 	if (c == '\n')
 		next_line = ft_substr(buffer, 0, pos + 1);
 	else
 		next_line = ft_substr(buffer, 0, pos);
+	printf("\033[0;36mNEXT_LINE: %s\n", next_line);
 	i = 0;
-	if (ft_strlen(buffer + pos, 0))
+	if (ft_strlen(buffer + pos + 1, 0))
 	{
-		while (buffer[pos + i])
+		while (buffer[pos + i + 1])
 		{
-			buffer[i] = buffer[pos + i];
+			buffer[i] = buffer[pos + i + 1];
 			i++;
 		}
 		ft_memset(buffer + i, 0, BUFFER_SIZE + 1 - i);
 	}
 	else
 		ft_memset(buffer, 0, BUFFER_SIZE + 1);
+	printf("\033[0;36mFINAL BUFFER: %s\n", buffer);
 	return (next_line);
 }
 
@@ -49,14 +53,21 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (0);
 	printf("\033[0;33mGET NEXT LINE :D\n");
-	next_line = 0;
+	next_line = malloc(1);
+	*next_line = 0;
 	if (ft_strlen(buffer, 0))
 	{
-		printf("\033[0;33mSOME IN BUFFER\n");
+		printf("\033[0;35mSOME IN BUFFER...\n");
 		if (ft_strchr(buffer, '\n'))
+		{
+			printf("\033[0;35m...WITH \\n\n");
 			next_line = read_buffer(buffer, '\n');
+		}
 		else
+		{
+			printf("\033[0;35m...WITHOUT \\n\n");
 			next_line = read_buffer(buffer, 0);
+		}
 	}
 	if (next_line && ft_strchr(next_line, '\n'))
 		return (next_line);
@@ -64,18 +75,6 @@ char	*get_next_line(int fd)
 	{
 		rd_chars = read(fd, buffer, BUFFER_SIZE);
 		printf("\033[0;34mBUFFER: %s\n", buffer);
-		if (rd_chars <= 0)
-		{
-			if (next_line)
-			{
-				mem_free = next_line;
-				next_line = ft_strjoin(next_line, "\n");
-				free(mem_free);
-				return (next_line);
-			}
-			return (0);
-		}
-		if (ft_strchr(buffer, '\n'))
 		while (rd_chars == BUFFER_SIZE && !ft_strchr(buffer, '\n'))
 		{
 			mem_free = next_line;
@@ -85,20 +84,50 @@ char	*get_next_line(int fd)
 			ft_memset(buffer, 0, BUFFER_SIZE + 1);
 			rd_chars = read(fd, buffer, BUFFER_SIZE);
 		}
-		if (rd_chars < BUFFER_SIZE && !ft_strchr(buffer, '\n'))
+		if (rd_chars < BUFFER_SIZE)
 		{
-			mem_free = next_line;
-			next_line = ft_strjoin(next_line, buffer);
-			free(mem_free);
-			mem_free = next_line;
-			next_line = ft_strjoin(next_line, "\n");
-			free(mem_free);
-			ft_memset(buffer, 0, BUFFER_SIZE + 1);
+			printf("\033[0;34mRD_CHARS < BUFFER_SIZE\n");
+			if (!ft_strlen(buffer, 0))
+			{
+				if(*next_line)
+				{
+					mem_free = next_line;
+					next_line = ft_strjoin(next_line, "\n");
+					free(mem_free);
+					return (next_line);
+				}
+				else
+					free(next_line);
+				return (0);
+			}
+			else
+			{
+				printf("\033[0;34mSOME IN BUFFER\n");
+				if (!ft_strchr(buffer, '\n'))
+				{
+					mem_free = next_line;
+					next_line = ft_strjoin(next_line, buffer);
+					free(mem_free);
+					mem_free = next_line;
+					next_line = ft_strjoin(next_line, "\n");
+					free(mem_free);
+					ft_memset(buffer, 0, BUFFER_SIZE + 1);
+				}
+				else
+				{
+						mem_free = next_line;
+						next_line = ft_strjoin(next_line, read_buffer(buffer, '\n'));
+						free(mem_free);
+				}
+			}
+		
 		}
 		else
 		{
+			printf("\033[0;34mFOUND A \\n IN BUFFER\n");
 			mem_free = next_line;
 			next_line = ft_strjoin(next_line, read_buffer(buffer, '\n'));
+			printf("\033[0;34mNEXT_LINE: %s\n", next_line);
 			free(mem_free);
 		}
 	}
